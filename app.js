84754068f05a7ac46f056f27df050b8c5e1b7395
KseniiaSeedelnykova <sedelnykova.ksenia@gmail.com>
2022-09-02 23:37:52 +0200

    JS

--- app.js
+++ app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecastHTML =
+    forecastHTML +
+    `
+              <div class="col-2">
+                <div class="weather-forecast-date">Fri</div>
+                <img
+                  src="http://openweathermap.org/img/wn/01d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max"></span>
+                    18°
+                  </span> 
+                  <span class="weather-forecast-temperature-min"></span>
+                  12°
+                  </span> 
+                </div>
+            </div>
+            `;
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -87,3 +116,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Warsaw");
+
+displayForecast;
